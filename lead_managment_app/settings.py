b64f756db529bd3c6fe5bc6d2ed6a42b3d6502f8
mirafzalswe <mirafzaldevelop@gmail.com>
2025-05-22 09:44:34 +0500

    postgresql installed

--- lead_managment_app/settings.py
+++ lead_managment_app/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'lead_managment_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'lead_management'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
